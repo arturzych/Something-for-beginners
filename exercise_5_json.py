feba269a4e9d7b84c594e73d52b97c4d3b38cab1
Artur <atrojmiasto@gmail.com>
2021-10-24 23:43:39 +0200

    My beginning programs

--- exercise_5_json.py
+++ exercise_5_json.py
@@ -1,10 +1,10 @@
 import json
 
 from exercise_3_dict import counts
-with open('hamlet.json', 'w+') as file_2:
+with open('hamlet.json', 'w') as file_2:
     a = 0
     for item in counts.items():
         a += 1
         json_text = json.dumps(item)
         if a <= 20:
-            file_2.write(f'{json_text}\n')
+            file_2.write(f'{a} {json_text}\n ')
