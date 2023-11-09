7d0e6b718939bb372d86250aa896db855dc0016e
artur <atrojmiasto77@gmail.com>
2023-11-09 22:58:28 +0100

    Simple regex | Grouping

--- regex.py
+++ regex.py
@@ -1,7 +1,11 @@
 import re
-txt = "120 PLNP"
-pattern = re.compile(r'PLN')
-
+txt = "#120 PLN"
+pattern = re.compile(r'PLN|#')
+# II sposób z wykorzystaniem podwójnego wzorca
+# pattern_2 = re.compile(r'#')
 print(txt)
 
-print(re.sub(pattern, '', txt))
+a = re.sub(pattern, '', txt)
+print(a)
+# II sposób
+# print(re.sub(pattern_2, '', a))
